4dbb949c64dc428915267125c051c0616645d72d
mynameishreya <shreyatripathi0301@gmail.com>
2024-12-30 11:30:08 +0530

    conersion

---
+++ conversion datatype.js
@@ -0,0 +1,54 @@
+// let score="33"
+// console.log(typeof(score))
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//conversion into number
+// let score="33abc"
+// console.log(typeof(score))
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let score=null
+// console.log(typeof(score))
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let score=undefined
+// console.log(typeof(score))
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let score=true
+// console.log(typeof(score))
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//33=>33
+//33abc=>nan
+//true=>1
+//false=>0
+
+//conversion into boolean
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn="shreya"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//string conversion
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let score=undefined
+console.log(typeof(score))
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
